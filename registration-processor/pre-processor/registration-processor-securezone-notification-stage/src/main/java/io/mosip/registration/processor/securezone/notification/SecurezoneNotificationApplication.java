99afd5aec2f9bbcde076796eaa7d59c46a49b513
kaledOu <kaled.ouali@gmail.com>
2025-08-11 22:37:23 +0100

    resolve conflict
    Signed-off-by: kaledOu <kaled.ouali@gmail.com>

--- registration-processor/pre-processor/registration-processor-securezone-notification-stage/src/main/java/io/mosip/registration/processor/securezone/notification/SecurezoneNotificationApplication.java
+++ registration-processor/pre-processor/registration-processor-securezone-notification-stage/src/main/java/io/mosip/registration/processor/securezone/notification/SecurezoneNotificationApplication.java
@@ -22,11 +22,6 @@ public class SecurezoneNotificationApplication {
                 "io.mosip.registration.processor.packet.manager.config",
                 "io.mosip.registration.processor.status.config", "io.mosip.registration.processor.rest.client.config",
                 "io.mosip.registration.processor.core.kernel.beans");
-<<<<<<< Updated upstream
-        System.out.println("branch ============================> MOSIP-18889-draft");
-=======
-
->>>>>>> Stashed changes
         configApplicationContext.refresh();
         LogManager.getLogManager().reset();
         Logger rootLogger = Logger.getLogger("");
